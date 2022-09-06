fdc6f409ce7076b4b0ba2f45f2fb57d6fe277929
sseyler0119 <sseyler0119@gmail.com>
2022-09-06 11:57:25 -0700

    Update game.js

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -1,9 +1,23 @@
 
 var buttonColors = ["red", "blue", "green", "yellow"]; // button colors
-var gamePattern = [];
-var userClickedPattern = [];
+var gamePattern = []; // array for computer generated game pattern
+var userClickedPattern = []; // array for user generated pattern
+var started = false; // var to track whether a game is being played, initialize to false
+var level = 0; // start level at 0
 
-// event listener
+/* event listeners */
+// detect when a key has been pressed on the keyborad, if it's the first time,
+// call nextSequence function
+$(document).keydown(function () {
+  // first key press
+  if(!started) {
+    $("#level-title").text("Level " + level); // display level
+    nextSequence();
+    started = true; // start game
+  }
+});
+
+// event listener to detect which button is clicked
 $(".btn").click(function () {
   // when "this" object is clicked, store the attribute id in userChosenColor
   var userChosenColor = $(this).attr("id");
@@ -11,10 +25,16 @@ $(".btn").click(function () {
   userClickedPattern.push(userChosenColor);
   animatePress(userChosenColor); // animate button
   playSound(userChosenColor); // play sound for chosen color
-})
+
+  var size = userClickedPattern.length; // get length of array
+  checkAnswer(size - 1); // pass in size - 1 as current level
+});
 
 // computer generated sequence
 function nextSequence() {
+    level++; // increment level
+    userClickedPattern = [];
+    $("#level-title").text("Level " +  level); // display level
     // generate a random number 0-3
     var randomNumber = Math.floor(Math.random() *4);
     // create randomChosenColor var, select random color from buttonColors
@@ -38,8 +58,41 @@ function playSound(color) {
 function animatePress(currentColor) {
   // target button by class, add pressed class to button selection
   $("." +  currentColor).addClass("pressed");
-  // remove class after .1 seconds
+  // remove class after 100 ms
   setTimeout(function() {
     $("." + currentColor).removeClass("pressed");
   }, 100);
+}
+
+/* check user answer against computer generated game pattern */
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel])
+  {
+    if(userClickedPattern.lenth === gamePattern.lenth)
+    {
+      // call next sequence function, 1000 ms delay
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  }
+  else
+  {
+    playSound("wrong"); // play sound
+    // apply game over class
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart"); // display message
+    // remove class game-over after 200 ms
+    setTimeout( function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    startOver(); // restart game
+  }
+}
+
+// reset values to start new game
+function startOver() {
+  level = 0;
+  gamePattern = []; // empty array
+  started = false;
 }
